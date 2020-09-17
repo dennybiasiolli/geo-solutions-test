1a08ea7666a2282a32d2763dac2e54f11242e354
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-09-17 11:26:50 +0200

    adding CoordinatesRequest model with related migration

--- src/geo/models.py
+++ src/geo/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
 
+NEAREST = 'nearest'
+FURTHEST = 'furthest'
+COORDINATE_CHOICES = (
+    (NEAREST, 'Nearest'),
+    (FURTHEST, 'Furthest'),
+)
+
+
 class Coordinate(models.Model):
     id_orig = models.IntegerField(unique=True)
     x = models.FloatField()
@@ -8,3 +16,17 @@ class Coordinate(models.Model):
 
     def __str__(self):
         return '{0}, {1}'.format(self.x, self.y)
+
+
+class CoordinatesRequest(models.Model):
+    owner = models.ForeignKey('auth.User',
+                              related_name='coordinates_requests',
+                              on_delete=models.CASCADE)
+    x = models.FloatField()
+    y = models.FloatField()
+    n = models.IntegerField()
+    operation = models.CharField(max_length=8, choices=COORDINATE_CHOICES)
+
+    def __str__(self):
+        return '{0} {1} to ({2}, {3})'.format(
+            self.n, self.operation, self.x, self.y)
