c2ce6c1676c09e85cd405e8bc3b23cec4c9b2d8c
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-09-17 09:04:36 +0200

    adding Coordinate model with related migration

--- src/geo/models.py
+++ src/geo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Coordinate(models.Model):
+    id_orig = models.IntegerField(unique=True)
+    x = models.FloatField()
+    y = models.FloatField()
+
+    def __str__(self):
+        return '{0}, {1}'.format(self.x, self.y)
